813132265cb281f28464dc7537a0d2e819c9e50d
Constantin Pape <c.pape@gmx.net>
2022-05-03 23:55:08 +0200

    Add Fig2 Panel B

--- views_for_paper.py
+++ views_for_paper.py
@@ -43,6 +43,39 @@ def panel_a():
     mobie.metadata.write_dataset_metadata(DS_FOLDER, metadata)
 
 
+def panel_b():
+    metadata = mobie.metadata.read_dataset_metadata(DS_FOLDER)
+    views = metadata["views"]
+    new_view = deepcopy(views["area3_tomos_37_38_40_41_54"])
+
+    # add the annotation display for fluo
+    fluo_sources = [[f"fluorescence-a3-c{i}" for i in range(3)]]
+    # note: table can have a superset of the sources in the annotation!
+    table_data = {"tsv": {"relativePath": "tables/fluorescence"}}
+    annotation_display = mobie.metadata.get_source_annotation_display(
+        "fluorescence-annotations", {str(ii): sources for ii, sources in enumerate(fluo_sources)},
+        table_data, ["default.tsv"], showAsBoundaries=True, boundaryThickness=1.0
+    )
+    new_view["sourceDisplays"].append(annotation_display)
+
+    # add the annotation display for lm-tomos (only select tomo38 lm)
+    tomo_sources = ["tomo_37_lm", "tomo_38_lm", "tomo_40_lm", "tomo_41_lm", "tomo_54_lm"]
+    # note: table can have a superset of the sources in the annotation!
+    table_data = {"tsv": {"relativePath": "tables/lm-tomogram-table"}}
+    annotation_display = mobie.metadata.get_source_annotation_display(
+        "lm-tomo-annotations", {str(ii): [source] for ii, source in enumerate(tomo_sources)},
+        table_data, ["default.tsv"], showAsBoundaries=True, boundaryThickness=1.0,
+        selectedAnnotationIds=["0;1"]  # tomo_38_lm has annotation_id 1
+    )
+    new_view["sourceDisplays"].append(annotation_display)
+
+    new_view["uiSelectionGroup"] = "paper"
+    views["Fig2_b"] = new_view
+    metadata["views"] = views
+    mobie.metadata.write_dataset_metadata(DS_FOLDER, metadata)
+
+
 if __name__ == "__main__":
-    panel_a()
+    # panel_a()
+    panel_b()
     mobie.validation.validate_dataset(DS_FOLDER, require_data=False)
